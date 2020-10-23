d03a222481fceb427b2a383365a4f9d5df96d7cf
Roman Khabarov <roman.khabarov@devexpress.com>
2020-10-23 16:58:59 +0300

    use SeriesDataAdapter

--- CS/FinancialChartExample/ViewModels/MainViewModel.cs
+++ CS/FinancialChartExample/ViewModels/MainViewModel.cs
@@ -3,17 +3,15 @@ using FinancialChartExample.Data;
 
 namespace FinancialChartExample.ViewModels {
     public class MainViewModel {
-        public CalculatedSeriesData CalculatedSeriesData { get; }
-        public XYSeriesData StockSeriesData { get; }
-        public XYSeriesData VolumeSeriesData { get; }
+        public StockPrices StockPrices { get; }
         public DateTimeRange VisualRange { get; }
 
         public MainViewModel() {
-            StockPrices stockPrices = StockData.GetStockPrices();
-            CalculatedSeriesData = new CalculatedSeriesData();
-            StockSeriesData = new XYSeriesData(stockPrices, SeriesDataType.Financial);
-            VolumeSeriesData = new XYSeriesData(stockPrices, SeriesDataType.DateTime);
-            VisualRange = new DateTimeRange() { VisualMin = new System.DateTime(2016, 06, 10), VisualMax = new System.DateTime(2016, 08, 29) };
+            StockPrices = StockData.GetStockPrices();
+            VisualRange = new DateTimeRange() {
+                VisualMin = new System.DateTime(2016, 06, 10),
+                VisualMax = new System.DateTime(2016, 08, 29)
+            };
         }
     }
 }
