99277c503e47bfd5c633f031de646f9d48eaf1b0
Roman Khabarov <roman.khabarov@devexpress.com>
2020-10-23 16:40:45 +0300

    view model small refactoring

--- CS/FinancialChartExample/ViewModels/MainViewModel.cs
+++ CS/FinancialChartExample/ViewModels/MainViewModel.cs
@@ -3,22 +3,17 @@ using FinancialChartExample.Data;
 
 namespace FinancialChartExample.ViewModels {
     public class MainViewModel {
-        readonly CalculatedSeriesData calculatedSeriesData;
-        readonly XYSeriesData stockSeriesData;
-        readonly XYSeriesData volumeSeriesData;
-        DateTimeRange visualRange;
-
-        public CalculatedSeriesData CalculatedSeriesData => calculatedSeriesData;
-        public XYSeriesData StockSeriesData => stockSeriesData;
-        public XYSeriesData VolumeSeriesData => volumeSeriesData;
-        public DateTimeRange VisualRange => visualRange;
+        public CalculatedSeriesData CalculatedSeriesData { get; }
+        public XYSeriesData StockSeriesData { get; }
+        public XYSeriesData VolumeSeriesData { get; }
+        public DateTimeRange VisualRange { get; }
 
         public MainViewModel() {
             StockPrices stockPrices = StockData.GetStockPrices();
-            calculatedSeriesData = new CalculatedSeriesData();
-            stockSeriesData = new XYSeriesData(stockPrices, DevExpress.XamarinForms.Charts.SeriesDataType.Financial);
-            volumeSeriesData = new XYSeriesData(stockPrices, DevExpress.XamarinForms.Charts.SeriesDataType.DateTime);
-            visualRange = new DateTimeRange() { VisualMin = new System.DateTime(2016, 7, 29), VisualMax = new System.DateTime(2016, 10, 15) };
+            CalculatedSeriesData = new CalculatedSeriesData();
+            StockSeriesData = new XYSeriesData(stockPrices, SeriesDataType.Financial);
+            VolumeSeriesData = new XYSeriesData(stockPrices, SeriesDataType.DateTime);
+            VisualRange = new DateTimeRange() { VisualMin = new System.DateTime(2016, 06, 10), VisualMax = new System.DateTime(2016, 08, 29) };
         }
     }
 }
